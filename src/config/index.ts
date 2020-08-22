f4d95055870bdcae921e01af8e57a1d95b12a082
Ali Furkan <a.furkan.2003@gmail.com>
2020-08-22 17:33:18 +0300

    🧹 Prettier

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,12 +1,12 @@
-import fs from "fs"
+import fs from "fs";
 
-const isProd = process.env.NODE_ENV === "production"
-if(isProd) {
+const isProd = process.env.NODE_ENV === "production";
+if (isProd) {
     const env = require("dotenv").parse(fs.readFileSync("prod.env"));
     for (const k in env) {
         process.env[k] = env[k];
     }
-} else require("dotenv").config()
+} else require("dotenv").config();
 
 export const Config = {
     PORT: Number(process.env.PORT) || Number(process.argv[2]) || 3000,
