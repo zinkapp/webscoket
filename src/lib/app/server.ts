4ebea889884ddd6554580cbde27f96955ea4248d
ali-furkan <46776661+ali-furkan@users.noreply.github.com>
2020-09-06 20:43:37 +0300

    Changed New ServerFactory

--- src/lib/app/server.ts
+++ src/lib/app/server.ts
@@ -2,40 +2,24 @@ import "reflect-metadata";
 import * as http from "http";
 import { Container } from "typedi";
 import { Logger } from "../logger";
+import { Module } from "./module";
+import { EVENTS } from "../constants";
 
 export class ServerFactory {
-    static async create(
+    static create(
         app: http.Server,
-        mod: any,
+        mod: typeof Zink.Module,
         adapter: Zink.Adapter,
         config?: Zink.ServerConfig,
-    ): Promise<SocketIO.Server> {
+    ): SocketIO.Server {
         const logger = Container.get(Logger);
-        logger.log("success", "Starting Application");
+        logger.log("start", "Starting Application");
         const io = adapter.create(app, config);
-        const moduleHandler = (mod) => {
-            const Mod: Zink.IModule = {
-                imports: Reflect.getMetadata("imports", mod) || [],
-                gateways: Reflect.getMetadata("gateways", mod) || [],
-                providers: Reflect.getMetadata("providers", mod) || [],
-            };
-            logger.log("success", `Mapped ${mod.name} Module`);
-            Mod.gateways.forEach((g) => {
-                const path: string = Reflect.getMetadata("path", g);
-                const events: {
-                    key: string;
-                    eventName: string;
-                }[] = Reflect.getMetadata("events", g);
-                adapter.gatewayHandler(io, { events, path, target: g });
-                logger.log("success", `Added ${g.name} Gateway {${path}}`);
-                events.forEach((e) =>
-                    logger.log("success", `Mapped {${e.eventName}} route`),
-                );
-            });
-            Mod.imports.forEach((i) => moduleHandler(i));
-        };
-        moduleHandler(mod);
-        logger.log("success", "Application successfully started");
+        io.on(EVENTS.CONNECTION, (socket: SocketIO.Socket) =>
+            socket.on(EVENTS.PING, () => socket.emit(EVENTS.PONG, true)),
+        );
+        Module.Handler(mod, adapter, io);
+        logger.log("start", "Application successfully started");
         return io;
     }
 }
