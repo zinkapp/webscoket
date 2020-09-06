71874d54964cebbd6cbbe1b1b145f00fa0dc650f
ali-furkan <46776661+ali-furkan@users.noreply.github.com>
2020-09-06 20:23:13 +0300

    Fixed Socket IO adapter

--- src/lib/adapters/io-adapter.ts
+++ src/lib/adapters/io-adapter.ts
@@ -2,9 +2,9 @@ import { Container } from "typedi";
 import * as IO from "socket.io";
 import * as http from "http";
 import { Logger } from "../logger";
-import { ErrorException } from "../exceptions/ErrorException";
-import { GatewayException } from "../exceptions/GatewayException";
+import { GatewayErrorException, GatewayException } from "../exceptions";
 import { EVENTS } from "../constants";
+import { AuthGuard } from "../auth/guard";
 
 export class SocketAdapter implements Zink.Adapter {
     private logger: Logger = Container.get(Logger);
@@ -20,16 +20,17 @@ export class SocketAdapter implements Zink.Adapter {
     public gatewayHandler(
         io: SocketIO.Server,
         gateway: {
-            target: any;
+            target: unknown;
             path: string;
             events: Array<{
                 key: string;
                 eventName: string;
             }>;
         },
-    ) {
+    ): void {
         const ga: Zink.Gateway = Container.get(gateway.target);
         const nsp = io.of(gateway.path);
+        nsp.use(AuthGuard);
         nsp.on(EVENTS.CONNECTION, (socket) => {
             socket.on(EVENTS.DISCONNECTION, () =>
                 typeof ga.onDisconnect === "function"
@@ -37,17 +38,18 @@ export class SocketAdapter implements Zink.Adapter {
                     : null,
             );
             socket.on(EVENTS.PONG, () => socket.emit(EVENTS.PONG, true));
-            this.eventHandler(socket, gateway.events, ga);
+            this.eventHandlers(socket, gateway.events, ga);
         });
     }
 
-    public eventHandler(
+    public eventHandlers(
         socket: SocketIO.Socket,
         events: { key: string; eventName: string }[],
         gateway: Zink.Gateway,
-    ) {
+    ): void {
         events.forEach(({ eventName, key }) => {
             socket.on(eventName, async (data) => {
+                const t = process.hrtime();
                 try {
                     const params = {
                         user: socket.user,
@@ -59,17 +61,37 @@ export class SocketAdapter implements Zink.Adapter {
                         throw new Error("Response Event Not Found");
                     if (!response.message)
                         throw new Error("Response Message Not Found");
+                    const time = process.hrtime(t);
+                    this.logger.log(
+                        "debug",
+                        `Request ${eventName} -> Response {${
+                            response.event
+                        }} in ${time[0]+time[1]/10**9}ms`,
+                    );
                     if (response.room)
                         return socket
                             .to(response.room)
                             .emit(response.event, response.message);
                     return socket.emit(response.event, response.message);
                 } catch (e) {
-                    if (e instanceof ErrorException)
-                        return socket.emit("error", e.desc);
-                    if (e instanceof GatewayException)
-                        return socket.emit(e.event, e.desc);
-                    return this.logger.log("error", e);
+                    const time = process.hrtime(t);
+                    if (e instanceof GatewayErrorException) {
+                        this.logger.log(
+                            "debug",
+                            `Request ${eventName} -> Response {${
+                                EVENTS.ERROR
+                            }} in ${time[0]+time[1]/10**9}ms`,
+                        );
+                        socket.error({ code: e.code, desc: e.desc });
+                    } else if (e instanceof GatewayException) {
+                        this.logger.log(
+                            "debug",
+                            `Request ${eventName} -> Response ${e.event} in ${
+                                time[0]+time[1]/10**9
+                            }ms`,
+                        );
+                        socket.emit(e.event, e.desc);
+                    } else this.logger.log("error", e);
                 }
             });
         });
