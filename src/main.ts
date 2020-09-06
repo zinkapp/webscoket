532b0da151cfd9cbe028a88c5bc4022f1867721d
ali-furkqn <mail@alifurkan.codes>
2020-09-06 20:22:00 +0300

    Configured Main File

--- src/main.ts
+++ src/main.ts
@@ -3,12 +3,21 @@ import { ServerFactory } from "./lib/app";
 import { AppModule } from "./module";
 import { Config } from "./config";
 import { SocketAdapter } from "./lib/adapters";
+import Container from "typedi";
+import { Logger } from "./lib/logger";
 
 async function main() {
-    const app = http.createServer();
-    const io = await ServerFactory.create(app, AppModule, new SocketAdapter());
+    const logger = Container.get(Logger);
+    const app = http.createServer((req,res)=>{
+        res.writeHead(404, {'Content-Type': 'application/json'});
+        res.write(`Cannot ${req.method.toUpperCase()} ${req.url}`)
+        res.end()
+    });
+    ServerFactory.create(app, AppModule, new SocketAdapter());
 
-    app.listen(Config.PORT);
+    await app.listen(Config.PORT, () => {
+        logger.log("start", `Application started at ${Config.PORT}`);
+    });
 }
 
 main();
