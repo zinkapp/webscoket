35254e56dcc84f3cd5ffb9cf1b68b021d2cd0880
ali-furkan <46776661+ali-furkan@users.noreply.github.com>
2020-09-06 20:34:58 +0300

    Fix: Module Decorator

--- src/lib/decorators/Module.ts
+++ src/lib/decorators/Module.ts
@@ -1,17 +1,16 @@
-import Container from "typedi";
-
 export const Module = (Module: Zink.IModule) => {
-    return (target) => {
-        if(Module.providers)
+    return (target: unknown): void => {
+        if (Module.providers)
             Module.providers.forEach((p) =>
-                Reflect.defineMetadata("imports", Module.imports||[], p),
+                Reflect.defineMetadata("imports", Module.imports || [], p),
             );
-        if(Module.gateways)
+        if (Module.gateways)
             Module.gateways.forEach((p) =>
-                Reflect.defineMetadata("providers", Module.providers||[], p),
+                Reflect.defineMetadata("providers", Module.providers || [], p),
             );
         Reflect.defineMetadata("imports", Module.imports || [], target);
         Reflect.defineMetadata("gateways", Module.gateways || [], target);
         Reflect.defineMetadata("providers", Module.providers || [], target);
+        Reflect.defineMetadata("exports", Module.exports || [], target);
     };
 };
