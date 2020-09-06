1f66c15fcf8744a418a8a52259e4c8e5de656eb3
ali-furkqn <mail@alifurkan.codes>
2020-09-06 20:45:10 +0300

    Removed socket.io Import

--- src/pool/pool.module.ts
+++ src/pool/pool.module.ts
@@ -1,8 +1,7 @@
-import IO from "socket.io";
 import { PoolGateway } from "./pool.gateaway";
 import { PoolService } from "./pool.service";
 import { Module } from "../lib/decorators";
-import {GameModule} from "../game/game.module"
+import { GameModule } from "../game/game.module";
 
 @Module({
     imports: [GameModule],
