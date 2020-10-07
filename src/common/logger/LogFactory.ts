da2458e2932cb71e1eebe95ad402e75c5d480fda
ali-furkqn <mail@alifurkan.codes>
2020-10-07 13:01:10 +0300

    A little adjustments

--- src/common/logger/LogFactory.ts
+++ src/common/logger/LogFactory.ts
@@ -1,5 +1,7 @@
 import * as moment from "moment";
 import * as chalk from "chalk";
+import Container from "typedi";
+import { COMMON } from "../constants";
 
 export interface LOG_CONFIG {
     displayDate?: boolean;
@@ -8,6 +10,7 @@ export interface LOG_CONFIG {
     type: string;
     typeColor: string;
     badge: string;
+    duration?: number;
 }
 
 const DEF_LOG_CONFIG: LOG_CONFIG = {
@@ -17,6 +20,7 @@ const DEF_LOG_CONFIG: LOG_CONFIG = {
     typeColor: "cyan",
     type: "success",
     badge: "",
+    duration: 0,
 };
 
 export class LogFactory {
@@ -25,13 +29,21 @@ export class LogFactory {
         Object.assign(this.config, config);
     }
     log(...msg: unknown[]): void {
-        const res: unknown[] = [chalk.yellow(`Flyz ${process.pid}`)];
+        const res: unknown[] = [
+            chalk.greenBright(
+                `[ Flyx${
+                    ` - ${Container.get<Zink.Adapter>(COMMON.ADAPTER).label}` ||
+                    ""
+                } ] ${chalk.yellow(process.pid)}`,
+            ),
+        ];
         if (this.config.displayDate)
             res.push(chalk.grey(`[${moment().format(this.config.typeDate)}]`));
         if (this.config.displayBadge)
             res.push(chalk[this.config.typeColor](this.config.badge));
         res.push(chalk.underline[this.config.typeColor](this.config.type));
         res.push(...msg);
+        res.push(chalk.yellow(`+ ${this.config.duration}ms`));
         console[`${this.config.type === "error" ? "error" : "log"}`](
             res.join(" "),
         );
