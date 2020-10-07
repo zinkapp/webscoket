14207e655b8f78e9fb5fe554b8b3bbb1a5d6b7dc
ali-furkqn <mail@alifurkan.codes>
2020-10-07 13:07:10 +0300

    Changed Adapter Structure

--- src/common/app/server.ts
+++ src/common/app/server.ts
@@ -3,22 +3,24 @@ import * as http from "http";
 import { Container } from "typedi";
 import { Logger } from "../logger";
 import { Module } from "./module";
-import { EVENTS } from "../constants";
+import { EVENTS, COMMON } from "../constants";
 
 export class ServerFactory {
     static create(
         app: http.Server,
         mod: typeof Zink.Module,
-        adapter: Zink.Adapter,
+        adapter: typeof Zink.Adapter,
         config?: Zink.ServerConfig,
     ): SocketIO.Server {
         const logger = Container.get(Logger);
-        logger.log("start", "Starting Application");
-        const io = adapter.create(app, config);
+        Container.set(COMMON.ADAPTER, new adapter());
+        const adapterInstance = Container.get<Zink.Adapter>(COMMON.ADAPTER);
+        logger.log("start", `Starting ${adapterInstance.label} Application`);
+        const io = adapterInstance.create(app, config);
         io.on(EVENTS.CONNECTION, (socket: SocketIO.Socket) =>
             socket.on(EVENTS.PING, () => socket.emit(EVENTS.PONG, true)),
         );
-        Module.Handler(mod, adapter, io);
+        Module.Handler(mod, adapterInstance, io);
         logger.log("start", "Application successfully started");
         return io;
     }
