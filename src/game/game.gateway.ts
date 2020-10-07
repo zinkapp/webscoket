d380a9885bffa1217f873591611cab5b22036115
ali-furkqn <mail@alifurkan.codes>
2020-10-07 13:17:05 +0300

    Added Action and Ready in Game Gateway

--- src/game/game.gateway.ts
+++ src/game/game.gateway.ts
@@ -1,12 +1,22 @@
-import { Event, Gateway } from "../lib/decorators";
-import { GameService } from "./game.service";
+import { Event, Gateway } from "../common/decorators";
+import { MatchService } from "./match.service";
 
 @Gateway("/game")
 export class GameGateway implements Zink.Gateway {
-    constructor(private gameService: GameService) {}
+    constructor(private matchService: MatchService) {}
 
     @Event("iam.ready")
     async iamReady(ctx: Zink.Match.Request): Promise<Zink.Response> {
-        return await this.gameService.iamReadyForMatch(ctx);
+        return await this.matchService.iamReadyForMatch(ctx);
+    }
+
+    @Event("match.action")
+    async createAction(ctx: Zink.Match.Request): Promise<Zink.Response> {
+        return this.matchService.action(
+            ctx.match.id,
+            ctx.user.id,
+            ctx.socket.to(`match.${ctx.match.id}`),
+            ctx.data,
+        );
     }
 }
